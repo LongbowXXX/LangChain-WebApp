e81a77f8d7ab4ffcc9be9edc7f03efced0e5ab36
LongbowXXX <50514445+LongbowXXX@users.noreply.github.com>
2024-02-11 19:21:11 +0900

    Add copyright

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+#  Copyright (c) 2024 LongbowXXX
+#
+#  This software is released under the MIT License.
+#  http://opensource.org/licenses/mit-license.php
+
 from flask import Flask
 
 app = Flask(__name__)
