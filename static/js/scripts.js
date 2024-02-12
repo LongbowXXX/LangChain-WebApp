f0475b4694fae88751475c5d37d2c8b76bac6d26
LongbowXXX <50514445+LongbowXXX@users.noreply.github.com>
2024-02-12 16:52:39 +0900

    Add style.

---
+++ static/js/scripts.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 LongbowXXX
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+$(document).ready(function () {
+    $("#query-form").on("submit", function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: "/agent",
+            type: "POST",
+            data: $(this).serialize(),
+            success: function (data) {
+                $("#history").append('<li class="user-input"><strong>User:</strong> ' + data.user_input + '</li>');
+                $("#history").append('<li class="system-response"><strong>Response:</strong> ' + data.response + '</li>');
+                $("#query").val('');
+            }
+        });
+    });
+});
