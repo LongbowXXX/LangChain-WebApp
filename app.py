1d2bbf495b9d55c37427b4c3f1c06d55b49a9596
LongbowXXX <50514445+LongbowXXX@users.noreply.github.com>
2024-02-12 16:11:52 +0900

    Use Ajax

--- app.py
+++ app.py
@@ -2,29 +2,32 @@
 #
 #  This software is released under the MIT License.
 #  http://opensource.org/licenses/mit-license.php
+import os
 
-from flask import Flask, request, session, render_template
+from flask import Flask, request, session, render_template, jsonify
 from flask.cli import load_dotenv
 
 from agent.agent_with_rag import run_agent
 
-# .flaskenvファイルの内容を読み込見込む
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'your secret key'  # セッションを使用するためには秘密鍵が必要です
+app.secret_key = os.urandom(24)
 
 
-@app.route('/', methods=['GET', 'POST'])
-async def agent():
-    if request.method == 'POST':
-        user_input = request.form['query']
-        response = await run_agent(user_input)
-        # ユーザーの入力とその応答をセッションに追加します
-        if 'history' not in session:
-            session['history'] = []
-        session['history'].append((user_input, response))
+@app.route('/', methods=['GET'])
+def form():
     return render_template('agent.html', history=session.get('history', []))
+
+
+@app.route('/agent', methods=['POST'])
+async def agent():
+    user_input = request.form['query']
+    response = await run_agent(user_input)
+    if 'history' not in session:
+        session['history'] = []
+    session['history'].append((user_input, response))
+    return jsonify(user_input=user_input, response=response)
 
 
 if __name__ == '__main__':
